895d215c0b97a2fd5327d79ce554652ee38ac85f
Frans Alexander <dev.norberg@gmail.com>
2022-08-05 15:17:58 +0200

    Add "paint brush" evntListener

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+createGrid(900, "grid_30x30");
 
-// toggle class hover to fill sqrs
+// Adds an eventListener to each square, making it so each square gets filled when moused over
+const sqr = document.querySelectorAll(".sqr");
+
+sqr.forEach( (square) => {
+
+    square.addEventListener("mousedown", (e) => {
+        e.target.style.background = "black";
+    });
+
+});
+ // how do i make it so the event starts onces clicked then goes over 
 
 
 function createGrid (gridSquares, gridSize) {
@@ -7,10 +18,12 @@ function createGrid (gridSquares, gridSize) {
  
     for(let i = 0; i < gridSquares; i++){
         const div = document.createElement("div");
-        div.classList.add(gridSize)
+        div.classList.add(gridSize);
+        div.classList.add("sqr");
         container.appendChild(div);
     }
 
+    
 }
 
-createGrid(900, "grid_30x30");
+
