c080fcb12e93aade073ed97ae963b10b979ccf32
Frans Alexander <dev.norberg@gmail.com>
2022-08-08 15:52:57 +0200

    Toggle event fill to no fill squares will toggle class to fill sqr with color, but event triggers again

--- script.js
+++ script.js
@@ -1,29 +1,51 @@
 createGrid(900, "grid_30x30");
 
-// Adds an eventListener to each square, making it so each square gets filled when moused over
-const sqr = document.querySelectorAll(".sqr");
+const squares = document.querySelectorAll(".sqr")
+activeateFillSqr()
 
-sqr.forEach( (square) => {
 
-    square.addEventListener("mousedown", (e) => {
-        e.target.style.background = "black";
+
+  function fillSqr(){
+    squares.forEach((square)=>{
+      square.addEventListener("mouseover", (e)=>{
+        e.target.classList.toggle("grid_colorFill")
+      })
     });
+  }
+  
+  function activeateFillSqr(){
+    squares.forEach((square)=>{
+      square.addEventListener("click", fillSqr);
+    });
+  }
 
-});
- // how do i make it so the event starts onces clicked then goes over 
 
 
-function createGrid (gridSquares, gridSize) {
-    const container = document.querySelector(".grid_container")
- 
-    for(let i = 0; i < gridSquares; i++){
-        const div = document.createElement("div");
-        div.classList.add(gridSize);
-        div.classList.add("sqr");
-        container.appendChild(div);
-    }
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ function createGrid (gridSquares, gridSize) {
+  const container = document.querySelector(".grid_container")
+
+  for(let i = 0; i < gridSquares; i++){
+      const div = document.createElement("div");
+      div.classList.add(gridSize);
+      div.classList.add("sqr");
+      container.appendChild(div);
+  }   
 }
-
-
