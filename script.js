ce2821f2a714caac0ae0763f45dca71582f352ab
Frans Alexander <dev.norberg@gmail.com>
2022-08-15 09:08:54 +0200

    Add random color generator

--- script.js
+++ script.js
@@ -4,19 +4,27 @@
 -----------------------------&-----------------------------------
 -------------------------VARIABLES-------------------------------
 -----------------------------------------------------------------*/
+
 createGrid(1024, "grid_32x32");
 
 const squares = document.querySelectorAll(".sqr")
 const colors = document.querySelectorAll(".color_picker");
+const random = document.querySelector(".random_color")
+
 let toggle = false;
+let toggleRandom = false; 
+let randomColor = `${getRandomColor()}`
 
 resetBtn()
-activateFillSquare()
 colorPickEffect()
+toggleRandomEvent()
+activateFillSquare()
+
+console.log(randomColor)
 
 
 
-/*---------------------------------------------------------------
+/*-------------------------------------------------------------
 ------------------------GRID-CREATION----------------------------
 ----------------------------------------------------------------*/
 
@@ -33,47 +41,64 @@ function createGrid (gridSquares, gridSize) {
     }   
 }
 
+
 /*---------------------------------------------------------------
-----------------------------COLOR--------------------------------
+----------------------------COLOR-PICK--------------------------------
 ----------------------------------------------------------------*/
 
 //CallBack function for toggling fillSquare()
 //Toggle is declared false in above variable. 
 //The toggleEvent() function turns "toggle variable" boolean value to opposite with (!)
-//with each click, toggling it from True to False
+//with each click, toggling it from True to False and Vice Versa
 
 function toggleEvent(){
   toggle = !toggle;
 };
 
-//Gets color, toggles fillSquare() and adds selected color to fillSquare()
+
+
+//Gets color, toggles fillSquare() and adds selected color to fillSquare() / fillSquareClick()
+
 
 function activateFillSquare(){
-  let col = "";
-   colors.forEach((color)=>{
-    color.addEventListener("click", (e)=>{
+  
+  let col = ""
+  colors.forEach((color)=>{
+    color.addEventListener("click", ()=>{
       col = color.getAttribute("style")
+      fillSquareClick(col)
     });
   });
   squares.forEach((square)=>{
     square.addEventListener("click", ()=>{
       toggleEvent();
       fillSquare(col);
+      console.log(toggle)
     });
   });
 };
+
 
 //Fill squares with color on mouseover if toggle is true
 
 function fillSquare(color){
   squares.forEach((square)=>{
     square.addEventListener("mouseover", (e)=>{
-      if(toggle == true) {
+      if(toggle == true) { 
         e.target.style.cssText = `${color}`
       };
     });
   });
 };
+
+function fillSquareClick(color){
+  squares.forEach((square)=>{
+    square.addEventListener("click", (e)=>{
+        e.target.style.cssText = `${color}`
+    });
+  });
+};
+
 
 //Highligths selected color and unselects previous selected color
 
@@ -83,6 +108,7 @@ function colorPickEffect (){
         colors.forEach((color)=>{
               color.classList.remove("active")
               e.target.classList.add("active")
+              random.classList.remove("random_color_effect")
         });
     });
   });
@@ -106,10 +132,65 @@ function resetBtn(){
 
 
 /*--------------------------------------------------------------
--------------------color picker palette----------------
+-------------------RANDOM-COLOR-PICKER--------------------------
 ----------------------------------------------------------------*/
 
 
 
+function getRandomColor(){
+  
+  var letters = '0123456789ABCDEF';
+  var color = 'background-color: #';
+
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color
+}
+
+function toggleRandomColor (){
+  toggleRandom = !toggleRandom
+}
+
+//Get getRandomColor(), run it each rtime mouse touches new square
+function fillSquareRandom(){
+  squares.forEach((square)=>{
+    square.addEventListener("mouseover", (e)=>{
+      if(toggleRandom == true) { 
+        e.target.style.cssText = getRandomColor()
+      };
+    });
+  });
+};
 
 
+function randomColorActivate(){
+  squares.forEach((square)=>{
+    square.addEventListener("click", ()=>{
+      toggleRandomColor();
+      fillSquareRandom();
+      console.log(toggleRandom)
+    });
+  });
+};
+
+
+// Calls Random color function and also makes toggle = false, Else if toggle = true it will color over squares with white
+function toggleRandomEvent(){
+  random.addEventListener("click", ()=>{
+    toggle = false
+    colorPickEffectRemove ()
+    randomColorActivate()
+    console.log(toggleRandom)
+       });
+}
+
+
+
+function colorPickEffectRemove (){
+        colors.forEach((color)=>{
+              color.classList.remove("active")
+        });
+        random.classList.add("random_color_effect")
+    }
+ 
